626442866683b6c544c50e0b51e05c71db829544
diego <diegoajardo0@gmail.com>
2026-06-30 19:50:40 -0300

    feat: adicionar controle de frequência e Git LFS

---
+++ .github/workflows/Frequencia.java
@@ -0,0 +1,12 @@
+public class Frequencia {
+
+    private int faltas;
+
+    public Frequencia(int faltas) {
+        this.faltas = faltas;
+    }
+
+    public int getFaltas() {
+        return faltas;
+    }
+}
